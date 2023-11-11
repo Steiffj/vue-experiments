39e599a66e44f9689901845b40150d87e4f52864
Steiffj <18404081+Steiffj@users.noreply.github.com>
2023-11-11 15:00:23 -0500

    Add Pixi route. Init Pixi app instance.

---
+++ src/views/PixiView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import * as PIXI from 'pixi.js';
+import { onMounted, ref } from 'vue';
+
+const renderTarget = ref<HTMLElement>(null as unknown as HTMLElement);
+
+onMounted(() => {
+  const app = new PIXI.Application<HTMLCanvasElement>({
+    antialias: true,
+    background: '#1099bb',
+    resizeTo: renderTarget.value
+  })
+  
+  renderTarget.value.appendChild(app.view);
+})
+</script>
+
+<template>
+  <div ref="renderTarget" class="render-target"></div>
+</template>
+
+<style scoped>
+.render-target {
+  width: 100%;
+  height: 100%;
+}
+</style>
